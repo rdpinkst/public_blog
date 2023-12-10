19763c0c182fdb995ab71e84d8f4fc664c69dc2e
Ryan P <rdpinkstaff@gmail.com>
2023-12-10 15:35:55 -0700

    Updated comment when comments query invalidated

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,15 +1,29 @@
 import { useLocation, Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import getPostComments from "../customHooks/getPostComments";
+import postAComment from "../customHooks/postAComment";
 import Comment from "./Comment";
 import WriteComment from "./WriteComment";
 
 
 
 function PostPage() {
+    const queryClient = useQueryClient();
     const location = useLocation();
     const {title, body, id } = location.state;
     const results = useQuery({queryKey: ["comments", id], queryFn: getPostComments});
+
+    const [name, setName] = useState("");
+    const [text, setText] = useState("");
+
+    const mutation = useMutation({
+        mutationFn: postAComment,
+        onSettled: () => {
+            queryClient.invalidateQueries({queryKey: ["comments", id]})
+        }
+    })
+    // console.log(mutation.variables)
 
     return (
         <div className="post-page">
@@ -22,7 +36,13 @@ function PostPage() {
               {results.data?.map((val) => {
                   return <Comment key= {val._id}  name= {val.username} comment= {val.comment} timeStamp={val.timeStamp} />
               })} 
-              <WriteComment id={id} />
+              <WriteComment 
+                id={id} 
+                mutation={mutation} 
+                name={name} 
+                setName = {setName} 
+                text={text}
+                setText={setText}/>
             </div>  
         </div>
     )
--- src/components/WriteComment.jsx
+++ src/components/WriteComment.jsx
@@ -1,59 +1,21 @@
-import { useState } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import postAComment from "../customHooks/postAComment";
-
-function WriteComment({  id, setComment }) {
-    const queryClient = useQueryClient();
-    const [name, setName] = useState("");
-    const [text, setText] = useState("");
-    // const API_POST_COMMENT = "https://holy-water-2894.fly.dev/api/v1/posts/" + id + "/comments"
-    const mutation = useMutation({
-        mutationFn: postAComment,
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ["comment", id]})
-        }
-    })
+function WriteComment({  id, mutation, name, setName, text, setText }) {
 
     function onSubmit(e) {
         e.preventDefault();
-        // postComment();
-        // if(name.length > 0 && text.length > 0) {
-        //     setComment(prevComment => [...prevComment, {username: name, comment: text, _id: Math.random() * 100, timeStamp: new Date().toLocaleDateString()}])
-        //     setName("");
-        //     setText("");
-        // }
-        console.log("submitted")
+        
         const data = {username: name, comment: text, postId: id};
-       console.log(mutation)
-        mutation.mutate(data, {
+        if(name.length > 0 && text.length > 0) {
+            mutation.mutate(data, {
             onSuccess: () => {
                 setName("");
                 setText("");
             }
         });
+        } else {
+            console.log("cant post")
+        }
+        
     }
-
-    // async function postComment() {
-    //     if(name.length > 0 && text.length > 0){
-    //       const data = {username: name, comment: text, postid: id};
-    //       try {
-    //           const res = await fetch(API_POST_COMMENT, {
-    //               method: "POST",
-    //               mode: "cors", 
-    //               headers: {
-    //                   "Content-Type": "application/json",
-    //               },
-    //               body: JSON.stringify(data),
-    //           });
-    //           const resData = await res.json();
-    //           return resData;
-    //       } catch(error) {
-    //           console.error(error);
-    //       }
-    //     } else {
-    //         alert("Name and comment needed to submit.")
-    //     }
-    // }
 
     return (
         <form onSubmit={onSubmit}>
